066d0b7ad816b1fe7d216e745129507f5d230917
Wookkie <nitritka93@gmail.com>
2024-07-10 00:09:25 +0300

    Добавлен файл main.go в cmd/url_shortener/

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	//"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"url_shortener/internal/config"
+	"url_shortener/internal/http-server/handlers/redirect"
+	"url_shortener/internal/http-server/handlers/url/save"
+	"url_shortener/internal/lib/logger/handlers/slogpretty"
+	"url_shortener/internal/lib/logger/sl"
+	"url_shortener/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env), slog.String("version", "15689")) //разобрать!!!!!!!!
+	log.Debug("debug messages are enabled")
+	log.Error("error messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("falied to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
+	router.Use(middleware.Logger)    // Логирование всех запросов
+	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
+	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
+
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage))
+		// TODO: add DELETE /url/{id}  если написали хендлер для удаления
+	})
+
+	router.Post("/url", save.New(log, storage)) //подключаем к роутеру только что написанный хендлер
+	router.Get("/{alias}", redirect.New(log, storage))
+	router.Delete("/url/{alias}", redirect.New(log, storage)) //?? мейби удалить, т.к. нет Delete - файла
+
+	log.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	//создаем сервер
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+	log.Error("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
